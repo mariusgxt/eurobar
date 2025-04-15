d3caeeb743380a01073652a9084280f74badf686
julian-bloennigen <bloennigen.julian@gmail.com>
2025-04-15 15:04:42 +0200

    Init

---
+++ backend/eurobar/src/main/java/com/eurobar/eurobar/EurobarApplication.java
@@ -0,0 +1,13 @@
+package com.eurobar.eurobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurobarApplication.class, args);
+	}
+
+}
---
+++ backend/eurobar/src/test/java/com/eurobar/eurobar/EurobarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurobar.eurobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
