288cf8b87f4720d7ff139b36c2ec427911775d7a
julian-bloennigen <bloennigen.julian@gmail.com>
2025-04-16 09:20:22 +0200

    Refactor CSS file structure and update import paths

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from '@lynx-js/react'
 
-import './App.css'
+import './css/App.css'
 import europeLogo from './assets/europeLogo.png'
 
 export function App() {
@@ -16,18 +16,16 @@ export function App() {
   }, [alterLogo])
 
   return (
-  
-      
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-            ? <image src={europeLogo} className='Logo--main' />
-            : <text>Felix ist ein Penner</text>}
-          </view>
-          <text className='Title'>EuroBar</text>
-          <text className='Hint'>*Not official</text>
-          </view>
-      </view>
+    <view className='App'>
+      <view className='Banner'>
+        <view className='Logo' bindtap={onTap}>
+          {alterLogo
+          ? <image src={europeLogo} className='Logo--main' />
+          : <text>Felix ist ein Penner</text>}
+        </view>
+        <text className='Title'>EuroBar</text>
+        <text className='Hint'>*Not official</text>
+        </view>
+    </view>
   )
 }
