869b3212e468350dd1bfea0133074eb90c19c308
julian-bloennigen <bloennigen.julian@gmail.com>
2025-04-30 19:05:42 +0200

    feat: update Spring Boot version to 3.4.5 and move the test controller on "/" to the controller folder

--- eurobar_backend/src/main/java/com/eurobar/eurobar_backend/EurobarBackendApplication.java
+++ eurobar_backend/src/main/java/com/eurobar/eurobar_backend/EurobarBackendApplication.java
@@ -2,20 +2,11 @@ package com.eurobar.eurobar_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class EurobarBackendApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurobarBackendApplication.class, args);
 	}
-
 }
