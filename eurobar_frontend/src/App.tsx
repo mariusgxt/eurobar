5226dba819bb5c35405b0315ba7ec3e72e09537e
julian-bl <a@gmail.com>
2025-06-02 21:05:59 +0200

    feat: initialize eurobar_frontend with React and Vite
    - Added package.json with dependencies for React, Vite, and TypeScript.
    - Created initial application structure including App component and styles.
    - Added assets including logos and SVG files.
    - Configured TypeScript with appropriate settings for app and node environments.
    - Set up Vite configuration for React.
    - Removed old App component from my-frontend.

---
+++ eurobar_frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Eurobar</h1>
+      <p className="read-the-docs">
+        Not official
+      </p>
+    </>
+  )
+}
+
+export default App
