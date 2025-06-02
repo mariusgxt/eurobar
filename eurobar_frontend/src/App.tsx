8be1e8f52da7552e69df06724f8a5b6201113812
Marius <mariusgutschalk@gmail.com>
2025-06-02 21:24:33 +0200

    Logo Fix & Moving of Assets

--- eurobar_frontend/src/App.tsx
+++ eurobar_frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <img src="/assets/europeLogo.png" alt="Eurobar Logo"/>
+      <img src="/assets/europeLogo.png" alt="Eurobar Logo" />
       <h1>Eurobar</h1>
       <p className="read-the-docs">
       Not official
