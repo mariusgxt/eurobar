d68c0b6ec624c4e7f44bdcd387f2b8166e9ebb68
Marius <mariusgutschalk@gmail.com>
2025-06-02 22:33:06 +0200

    feat: added Buttons to either let user type or scan the Barcodes

--- eurobar_frontend/src/Scanner.tsx
+++ eurobar_frontend/src/Scanner.tsx
@@ -1,6 +1,27 @@
+import { useRef } from 'react';
 import './App.css'
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleTypeClick = () => {
+    const input = window.prompt('Please enter the Barcode:');
+    if (input !== null) {
+      console.log('User input:', input);
+    }
+  };
+
+  const handleScanClick = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+    } catch (err) {
+      alert('Camera access denied or not available.');
+    }
+  };
 
   return (
     <>
@@ -8,9 +29,11 @@ function App() {
       <h1>
         Eurobar
       </h1>
-      <p className="read-the-docs">
-        Click here to Scan
-      </p>
+      <button onClick={handleTypeClick}>Click here to type in the Barcode</button>
+      <button onClick={handleScanClick}>Click here to Scan the Barcode</button>
+      <div>
+        <video ref={videoRef} style={{ width: '300px', marginTop: '20px' }} autoPlay />
+      </div>
     </>
   )
 }
